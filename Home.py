1cddd217cbbf218e916ff01972c771653e7611ce
daveg7lee <daveg7lee@gmail.com>
2023-11-22 16:34:12 +0900

    Chat Messages

---
+++ Home.py
@@ -0,0 +1,22 @@
+import streamlit as st
+
+st.set_page_config(page_title="Fullstack GPT", page_icon="🦜")
+
+st.title("Fullstack GPT Home")
+
+st.markdown(
+    """
+# Hello!
+            
+Welcome to my FullstackGPT Portfolio!
+            
+Here are the apps I made:
+            
+- [ ] [DocumentGPT](/DocumentGPT)
+- [ ] [PrivateGPT](/PrivateGPT)
+- [ ] [QuizGPT](/QuizGPT)
+- [ ] [SiteGPT](/SiteGPT)
+- [ ] [MeetingGPT](/MeetingGPT)
+- [ ] [InvestorGPT](/InvestorGPT)
+"""
+)
