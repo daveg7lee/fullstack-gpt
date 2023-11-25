3cbb15b8532db0b087ad98ab1d8f1603dc6c1cb2
daveg7lee <daveg7lee@gmail.com>
2023-11-25 20:04:32 +0900

    Function Calling

--- pages/03_QuizGPT.py
+++ pages/03_QuizGPT.py
@@ -9,19 +9,50 @@ from langchain.callbacks import StreamingStdOutCallbackHandler
 from langchain.schema import BaseOutputParser
 
 
-class JsonOutputParser(BaseOutputParser):
-    def parse(self, text: str):
-        text = text.replace("```", "").replace("json", "")
-        return json.loads(text)
-
-
-output_parser = JsonOutputParser()
-
 st.set_page_config(page_title="QuizGPT", page_icon="❓")
 
 
 def format_docs(docs):
     return "\n\n".join(document.page_content for document in docs)
+
+
+function = {
+    "name": "create_quiz",
+    "description": "function that takes a list of questions and answers and returns a quiz",
+    "parameters": {
+        "type": "object",
+        "properties": {
+            "questions": {
+                "type": "array",
+                "items": {
+                    "type": "object",
+                    "properties": {
+                        "question": {
+                            "type": "string",
+                        },
+                        "answers": {
+                            "type": "array",
+                            "items": {
+                                "type": "object",
+                                "properties": {
+                                    "answer": {
+                                        "type": "string",
+                                    },
+                                    "correct": {
+                                        "type": "boolean",
+                                    },
+                                },
+                                "required": ["answer", "correct"],
+                            },
+                        },
+                    },
+                    "required": ["question", "answers"],
+                },
+            }
+        },
+        "required": ["questions"],
+    },
+}
 
 
 llm = ChatOpenAI(
@@ -30,6 +61,13 @@ llm = ChatOpenAI(
     streaming=True,
     callbacks=[
         StreamingStdOutCallbackHandler(),
+    ],
+).bind(
+    function_call={
+        "name": "create_quiz",
+    },
+    functions=[
+        function,
     ],
 )
 
@@ -44,25 +82,7 @@ questions_prompt = ChatPromptTemplate.from_messages(
     Based ONLY on the following context make 10 (TEN) questions minimum to test the user's knowledge about the text.
 
     Each question should have 4 answers, three of them must be incorrect and one should be correct.
-            
-    Use (o) to signal the correct answer.
-            
-    Question examples:
-            
-    Question: What is the color of the ocean?
-    Answers: Red|Yellow|Green|Blue(o)
-            
-    Question: What is the capital or Georgia?
-    Answers: Baku|Tbilisi(o)|Manila|Beirut
-            
-    Question: When was Avatar released?
-    Answers: 2007|2001|2009(o)|1998
-            
-    Question: Who was Julius Caesar?
-    Answers: A Roman Emperor(o)|Painter|Actor|Model
-            
-    Your turn!
-            
+
     Context: {context}
     """,
         )
@@ -71,130 +91,6 @@ questions_prompt = ChatPromptTemplate.from_messages(
 
 
 questions_chain = {"context": format_docs} | questions_prompt | llm
-
-formatting_prompt = ChatPromptTemplate.from_messages(
-    [
-        (
-            "system",
-            """
-    You are a powerful formatting algorithm.
-
-    You format exam questions into JSON format.
-    Answers with (o) are the correct ones.
-
-    Example Input:
-    Question: What is the color of the ocean?
-    Answers: Red|Yellow|Green|Blue(o)
-
-    Question: What is the capital or Georgia?
-    Answers: Baku|Tbilisi(o)|Manila|Beirut
-
-    Question: When was Avatar released?
-    Answers: 2007|2001|2009(o)|1998
-
-    Question: Who was Julius Caesar?
-    Answers: A Roman Emperor(o)|Painter|Actor|Model
-
-
-    Example Output:
-     
-    ```json
-    {{ "questions": [
-            {{
-                "question": "What is the color of the ocean?",
-                "answers": [
-                        {{
-                            "answer": "Red",
-                            "correct": false
-                        }},
-                        {{
-                            "answer": "Yellow",
-                            "correct": false
-                        }},
-                        {{
-                            "answer": "Green",
-                            "correct": false
-                        }},
-                        {{
-                            "answer": "Blue",
-                            "correct": true
-                        }},
-                ]
-            }},
-                        {{
-                "question": "What is the capital or Georgia?",
-                "answers": [
-                        {{
-                            "answer": "Baku",
-                            "correct": false
-                        }},
-                        {{
-                            "answer": "Tbilisi",
-                            "correct": true
-                        }},
-                        {{
-                            "answer": "Manila",
-                            "correct": false
-                        }},
-                        {{
-                            "answer": "Beirut",
-                            "correct": false
-                        }},
-                ]
-            }},
-                        {{
-                "question": "When was Avatar released?",
-                "answers": [
-                        {{
-                            "answer": "2007",
-                            "correct": false
-                        }},
-                        {{
-                            "answer": "2001",
-                            "correct": false
-                        }},
-                        {{
-                            "answer": "2009",
-                            "correct": true
-                        }},
-                        {{
-                            "answer": "1998",
-                            "correct": false
-                        }},
-                ]
-            }},
-            {{
-                "question": "Who was Julius Caesar?",
-                "answers": [
-                        {{
-                            "answer": "A Roman Emperor",
-                            "correct": true
-                        }},
-                        {{
-                            "answer": "Painter",
-                            "correct": false
-                        }},
-                        {{
-                            "answer": "Actor",
-                            "correct": false
-                        }},
-                        {{
-                            "answer": "Model",
-                            "correct": false
-                        }},
-                ]
-            }}
-        ]
-     }}
-    ```
-    Your turn!
-    Questions: {context}
-""",
-        )
-    ]
-)
-
-formatting_chain = formatting_prompt | llm
 
 
 @st.cache_data(show_spinner="Loading file...")
@@ -215,8 +111,9 @@ def split_file(file):
 
 @st.cache_data(show_spinner="Making quiz...")
 def run_quiz_chain(_docs, topic):
-    chain = {"context": questions_chain} | formatting_chain | output_parser
-    return chain.invoke(_docs)
+    response = questions_chain.invoke(_docs)
+    response = response.additional_kwargs["function_call"]["arguments"]
+    return json.loads(response)
 
 
 @st.cache_data(show_spinner="Searching Wikipedia...")
